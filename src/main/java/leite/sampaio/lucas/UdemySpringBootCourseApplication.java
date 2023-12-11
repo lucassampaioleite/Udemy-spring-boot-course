4268928849ba8b03f43f5c4c271e8144d7e1b9e7
lucassampaioleite <lucas.sampaio.leite@gmail.com>
2023-12-11 15:26:26 -0300

    spring boot project created

---
+++ src/main/java/leite/sampaio/lucas/UdemySpringBootCourseApplication.java
@@ -0,0 +1,13 @@
+package leite.sampaio.lucas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringBootCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringBootCourseApplication.class, args);
+	}
+
+}
